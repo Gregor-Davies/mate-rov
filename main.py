1a44b4db784604e2b74d39a4cd03a700e13c66d3
RobertTurkot <thiswarjustgottenfeethigh@gmail.com>
2023-02-08 00:50:03 +0000

    more controller parsing

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ x = XInput
 
 
 
-#L thumb - rov controls
+#L thumb - rov rotation and movement
 #l thumb button - lock for rov
-#d pad - rotation
 #r thumb camera
 #back - disable gyro stablization
 #RTrigger - Up
@@ -20,36 +19,53 @@ Buttons output:
 """
 
 timeout = 0 #timer that quits the program if the controller disconects for a certain amount of time
-max_rotation = 1 #max amount of degrees that the ROV can rotate in 1ms
+max_rotation = 1 #max rotational velocity of the ROV in degrees/s in 1ms
+max_elevation = 1 #max amount of m/s that the ROV can go up/down by in 1ms
 
 #Initialisation of variables for the buttons
 A = False
-Aprepressed = False
+Apressed = False
 B = False
-Bprepressed = False
+Bpressed = False
 Y = False
-Yprepressed = False
+Ypressed = False
 X = False
-Xprepressed = False
+Xpressed = False
+
+
 
 while True:
     state = x.get_state(0)
    
     thumbsticks = x.get_thumb_values(state)
-    buttons = x.get_button_values(state)
+    switches = x.get_button_values(state)
     triggers = x.get_trigger_values(state)
 
+
+
+
     rotation = thumbsticks[0][0]*max_rotation #how much to rotate the ROV by every 1ms
-    elevation = [triggers[0], triggers[1]] #Up-Down speed. pressing both triggers is frasers issue
+    velocity = thumbsticks[0][1] #Velocity of the ROV (Basically controls PMW of the motors for forwards and backwards movement)
+    elevation = [triggers[0]*max_elevation triggers[1]*max_elevation] #Up-Down speed. pressing both triggers is frasers issue
 
-    if buttons["Y"] == False:
-        Yprepressed = False
+    if switches["LEFT_THUMB"] == False:
+        lthumbpressed
 
-    if buttons["Y"] == True and Yprepressed == False:
+    if switches["Y"] == False:
+        Ypressed = False
+
+    if switches["Y"] == True and Yprepressed == False:
         Y = not Y
         Yprepressed = True
+
+    
+
+
+
+
+    buttons = [X, Y, A, B]
         
-    print(Y)
+    tosend = [velocity, rotation, elevation, buttons, movelock]
 
 
 
